1b955038db7d9dee86fdd060bca6613b7d890918
김영도 <rladudeh98@naver.com>
2024-11-29 13:39:18 +0900

    20241129 Project Setting

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="auto_trading",
+    name="BinaceFutureTrading",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "python-binance",
-        "pandas",
-        "numpy"
     ],
 )
